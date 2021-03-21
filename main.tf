3b0bfa542caf5e24e3a9164aa100fb54c0cadce0
Jonnychipz <john@luinn-ltd.co.uk>
2021-03-21 13:35:09 +0000

    remote backend added

--- main.tf
+++ main.tf
@@ -3,11 +3,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.52.0"
     }
   }
-}
+  backend "azurerm" {
+    resource_group_name   = "jonnychipz-state"
+    storage_account_name  = "jonnychipztfstate"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+    }
 
+}
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
